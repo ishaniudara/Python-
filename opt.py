7fc2caabe09ffe2406465117df7994179f404fd0
DVIUdara <ishani.udara@akvasoft.com>
2021-01-13 21:07:17 +0530

    operators

---
+++ opt.py
@@ -0,0 +1,51 @@
+# arithmetic
+a=6
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+# assignemt
+x=3
+x=x+3
+
+print(x)
+
+# comparision
+print(a==b)
+print(a!=b)
+print(a<b)
+
+# logical
+print(a>b and x<a)
+print(a<b or x>b)
+
+# identity
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+
+# returns True because z is the same object as x
+
+print(x is y)
+
+# returns False because x is not the same object as y, even if they have the same content
+
+print(x == y)
+
+# to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
+
+# membership
+x = ["apple", "banana"]
+
+print("appl" in x)
+print("apple" not in x)
+
+# bitwise
+
+print(a & b)
+print ( a|b)
